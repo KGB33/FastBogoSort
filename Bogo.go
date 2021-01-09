cf2305554c3efb1ac4bf3589fdd295202c9436e7
KGB33 <github@bassingthwaite.org>
2021-01-09 10:31:34 -0700

    added cpu info func, updated comments

--- Bogo.go
+++ Bogo.go
@@ -1,9 +1,13 @@
 package FastBogo
 
 import (
+	"io/ioutil"
+	"log"
 	"math/rand"
 	"runtime"
 	"sort"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -11,7 +15,8 @@ import (
 // the given maxes.
 // maxMem: Maximum Bytes of memory Allocated;
 // maxCPU: Maximum Percent CPU Useage (0 to 100);
-// delay: When one of the above values is exceed, how much time should we wait before calling garbage collection?
+// delay: When one of the above values is exceed,
+// how much time should we wait before calling garbage collection?
 //
 // 	This function calls the garbage collector to clear up compleated goroutines
 // 	once maxCPU or maxMem is exceed.
@@ -55,11 +60,43 @@ func Sort(a []int, out chan []int) {
 	}
 }
 
+// Uses the `/proc` file system to get real-time
+// CPU stats. see `man 5 proc` for more info.
+//
+// Also see this SO post for more info
+// https://stackoverflow.com/a/17783687
+//
+// NOTE: I'm assuming that the order is consistant.
 func currCPUUse() int {
-	// TODO
-	return 0
+	rawData, err := ioutil.ReadFile("/proc/stat")
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := string(rawData)
+	cpuData := strings.Fields(strings.Split(data, "\n")[0]) // CPU is the 1st line
+	if cpuData[0] != "cpu" {
+		log.Fatal("Incorrectly parsed `/proc/stat`")
+	}
+	var total, idle float64
+	for i, v := range cpuData[1:] {
+		val, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		total += val
+		if i == 3 { // 4th field is idle
+			idle = val
+		}
+	}
+	return int(100 * ((total - idle) / total))
 }
 
+// For some reason the memory allocated can
+// be greater than the total physical memory on
+// the system. (Even with no swap)
+// As such:
+// TODO: Change this to use /proc/self/stat
+// Much like the currCPUUse function
 func currMemoryAlloc() int {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
